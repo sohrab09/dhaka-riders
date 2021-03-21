f44f27fb80511a73c10b14e0fb82fed3b8ef605b
Sohrab Hossain <sohrab.cse9@gmail.com>
2021-03-21 22:09:53 +0600

    97% done

--- src/components/Ride/Ride.js
+++ src/components/Ride/Ride.js
@@ -6,14 +6,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons'
 
 const Ride = (props) => {
-    const {name, image } = props.ride;
+    const {id, name, image } = props.ride;
     return (
 
         <Card as={Link} to="/destination" className="cardStyle shadow-sm p-2 bg-body rounded container " style={{ width: '14rem' }}>
             <Card.Img variant="top" src={image} />
             <Card.Body>
                 <Card.Title className="text-danger">{name}</Card.Title>
-                <Button as={Link} to="/destination" variant="success">Book Now <FontAwesomeIcon icon={faCheckCircle}/></Button>
+                <Button as={Link} to="destination" variant="success">Book Now <FontAwesomeIcon icon={faCheckCircle}/></Button>
             </Card.Body>
         </Card>
 
